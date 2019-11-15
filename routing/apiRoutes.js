9776c3c4b63f6bf3f972d8ed1593e9a99f40e0cb
ErikBoerma <eboerma@gmail.com>
2019-11-15 14:09:32 -0500

    updated api with function to find bestie

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -4,5 +4,30 @@ module.exports = function(app){
     app.get("/api/friends", function(req, res){
         res.json(friendData)
     });
-    
+    app.post("/api/friends", function(req, res){
+        var newFriendScoreArr = req.body.scores;
+        var scoreArr = [];
+        var newFriendScoreTotal = 0;
+
+        for (var i =0; i < friendData.length; i++){
+            currentFriend = friendData[i];
+            
+            var friendScore = 0;
+            for (var j = 0; j < currentFriend.scores.length; j++){
+                friendScore += (Math.abs(parseInt(currentFriend.scores[j])- parseInt(newFriendScoreArr)));
+            }
+            scoreArr.push(friendScore);
+        }
+        for (var k = 0; k < scoreArr.length; k++){
+            if (scoreArr[k] <= scoreArr[newFriendScoreTotal]){
+                newFriendScoreTotal = k;
+            }
+        }
+
+        var newBestie = friendData[newFriendScoreTotal];
+        res.json(newBestie);
+        friendData.push(req.body);
+
+
+    })   
 }
--- routing/htmlRoutes.js
+++ routing/htmlRoutes.js
@@ -5,8 +5,8 @@ module.exports = function (app) {
         res.sendFile(path.join(__dirname, "../public/survey.html"))
     })
 
-    app.get("*", function (req, res) {
-        res.sendFile(Path.join(__dirname, "../public/home.html"))
+    app.use("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"))
         
     })
 }
